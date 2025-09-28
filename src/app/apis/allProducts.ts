f5c9c1a418693350d8912faab59208070a462d49
korolus <kerolosadly59@gmail.com>
2025-09-28 15:22:39 +0300

    update to deploy

--- src/app/apis/allProducts.ts
+++ src/app/apis/allProducts.ts
@@ -1,6 +1,6 @@
 export default async function getAllProducts(){
     
-  const response = await fetch("http://localhost:3000/api/users", {
+  const response = await fetch("https://ecommerce.routemisr.com/api/v1/products", {
     cache: "force-cache"
   })
 
